9bedee75fa7e7c5b9d370d1380d00914073075f7
king-beard <chucho_herrera10@icloud.com>
2026-01-27 20:19:35 -0600

    hasmap - countFrecuencies.

---
+++ DataStructuresAndAlgorithms/DataStructures/Dictionary/Exercises.cs
@@ -0,0 +1,19 @@
+﻿namespace DataStructuresAndAlgorithms.DataStructures.Dictionary
+{
+    public static class Exercises
+    {
+        public static void CountFrequencies(int[] nums)
+        {
+            Dictionary<int, int> frecuencies = [];
+
+            for (int i = 0; i < nums.Length; i++) 
+                if (frecuencies.TryGetValue(nums[i], out int value))
+                    frecuencies[nums[i]] = ++value;
+                else
+                    frecuencies[nums[i]] = 1;
+
+            foreach (KeyValuePair<int, int> num in frecuencies)
+                Console.WriteLine($"{num.Key} -> {num.Value}");
+        }
+    }
+}
--- DataStructuresAndAlgorithms/Program.cs
+++ DataStructuresAndAlgorithms/Program.cs
@@ -1,8 +1,13 @@
 ﻿using DataStructuresAndAlgorithms.DataStructures.Arrays;
+using DataStructuresAndAlgorithms.DataStructures.Dictionary;
 
-int[] nums = [2, 7, 11, 15];
+int[] nums = { 2, 7, 11, 15 };
 int target = 9;
 
 int[] index = SolutionTwoSum.TwoSumOptimized(nums, target);
 
 Console.WriteLine($"[{index[0]}, {index[1]}]");
+
+int[] numss = { 1, 2, 2, 3, 3, 3 };
+
+Exercises.CountFrequencies(numss);
