99d62ea7b06e75d643fff55cf76013b14d5bb061
king-beard <chucho_herrera10@icloud.com>
2026-01-28 00:35:19 -0600

    add DataStructuresAndAlgorithms.Test

---
+++ DataStructuresAndAlgorithms.Test/DataStructures/Arrays/ExercisesTest.cs
@@ -0,0 +1,37 @@
+﻿using DataStructuresAndAlgorithms.DataStructures.Arrays;
+
+namespace DataStructuresAndAlgorithms.Test.DataStructures.Arrays
+{
+    public class ExercisesTest
+    {
+        [Fact]
+        public void TwoSum()
+        {
+            // Arrange
+            int[] nums = [15, 2, 11, 7];
+            int target = 9;
+
+            // Act
+            int[] index = SolutionTwoSum.TwoSum(nums, target);
+
+            // Assert
+            Assert.Equal(1, index[0]);
+            Assert.Equal(3, index[1]);
+        }
+
+        [Fact]
+        public void TwoSumOptimized()
+        {
+            // Arrange
+            int[] nums = [15, 2, 11, 7];
+            int target = 9;
+
+            // Act
+            int[] index = SolutionTwoSum.TwoSumOptimized(nums, target);
+
+            // Assert
+            Assert.Equal(1, index[0]);
+            Assert.Equal(3, index[1]);
+        }
+    }
+}
---
+++ DataStructuresAndAlgorithms.Test/DataStructures/Dictionary/ExercisesTest.cs
@@ -0,0 +1,21 @@
+﻿using DataStructuresAndAlgorithms.DataStructures.Dictionary;
+
+namespace DataStructuresAndAlgorithms.Test.DataStructures.Dictionary
+{
+    public class ExercisesTest
+    {
+        [Fact]
+        public void CountFrequencies()
+        {
+            // Arrange
+            int[] nums = { 1, 2, 2, 3, 3, 3 };
+
+            // Act
+            Dictionary<int, int> frecuencies = Exercises.CountFrequencies(nums);
+            frecuencies.TryGetValue(3, out int ThreeKeyValue);
+
+            // Assert
+            Assert.Equal(3, ThreeKeyValue);
+        }
+    }
+}
